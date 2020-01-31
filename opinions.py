af3879eb8b33bfc49aa36d6142831bc560e1df63
Richard Louden <richard.louden@theoaklandgroup.co.uk>
2020-01-31 13:03:12 +0000

    added new opinion endpoint

--- opinions.py
+++ opinions.py
@@ -7,31 +7,49 @@ from random import choice
 app = Flask(__name__)
 
 opinions = ["Looks good", "Ummm I'm not really sure", "Ask Tom", "That looks stupid", "I'd do it a different way", "Looks great!"]
+new_opinions = []
+
 
 @app.route('/add-opinion', methods=['GET', 'POST'])
-def add_opinion(opinions = opinions):
+def add_opinion(opinions = opinions, new_opinions = new_opinions):
 
     opinion = request.args.get('opinion')
 
     new_op = str(opinion).replace("_", " ")
 
-    opinions = opinions.append(new_op)
+    if new_op not in opinions:
 
-    return opinions
-    
+        opinions.append(new_op)
+        new_opinions.append(new_op)
+
+        return f'<h1 style = "text-align: center">You have added {new_op}</h1>'
+
+    else:
+       return f'<h1 style = "text-align: center">{new_op} is already included</h1>' 
+
 
 @app.route('/opinion')
 def opinion_func(opinions = opinions):
 
     opinion = choice(opinions)
 
-    return f"<h1>{opinion}</h1>"
+    return f'<h1 style = "text-align: center">{opinion}</h1>'
 
 
-@app.route('/list-opinions')
-def list_opinions(opinions = opinions):
+@app.route('/reset-opinions')
+def reset_opinions(opinions = opinions, new_opinions = new_opinions):
 
-    print(f"<h1>There's a choice of {len(opinions)} opinions</h1>")
+    for i in new_opinions:
+
+        opinions.pop(i)
+
+    return f'<h1 style = "text-align: center"> Reset to original opinions!</h1>'
+
+
+@app.route('/num-opinions')
+def num_opinions(opinions = opinions):
+
+    return(f'<h1 style = "text-align: center">There is a choice of {len(opinions)} opinions</h1>')
     
 if __name__ == '__main__':
     if 'PORT' in os.environ:
