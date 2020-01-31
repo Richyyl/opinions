b092d25ad5ee71868744fb4b669540f2a4ab42cd
Richard Louden <richard.louden@theoaklandgroup.co.uk>
2020-01-31 11:20:25 +0000

    added new opinion endpoint

--- opinions.py
+++ opinions.py
@@ -8,21 +8,28 @@ app = Flask(__name__)
 
 opinions = ["Looks good", "Ummm I'm not really sure", "Ask Tom", "That looks stupid", "I'd do it a different way", "Looks great!"]
 
-@app.route('/add-opinion/<opinion>', methods = ['get'])
-def add_opinion(opinion, opinions = opinions):
+@app.route('/add-opinion', methods = ['get'])
+def add_opinion(opinions = opinions):
 
-    new_op = opinion
+    opinion = request.args.get('opinion')
 
-    new_op = str(new_op).replace("_", " ")
+    new_op = str(opinion).replace("_", " ")
 
     opinions = opinions.append(new_op)
 
     return opinions
     
+
 @app.route('/opinion')
 def opinion_func(opinions = opinions):
 
     opinion = choice(opinions)
+
+    return f"<h1>{opinion}</h1>"
+
+
+@app.route('/list-opinions')
+def opinion_func(opinions = opinions):
 
     return f"<h1>{opinion}</h1>"
     
